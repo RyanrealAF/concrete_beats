934eca80e4df2e83856c6210bfa9f3a8bc192d40
purarecoveryryan <purarecoveryryan@gmail.com>
2026-03-30 22:41:16 +0000

    fgg

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: './',
+    base: '/concrete_beats/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
